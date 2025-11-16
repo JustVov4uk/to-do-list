8fa5fb44e3b5c7f7bd9ae85d0e27a6e1be2aaf63
JustVov4uk <volodabudzan4@gmail.com>
2025-11-16 10:59:51 +0100

    register models in admin.py

--- config/admin.py
+++ config/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from config.models import Tag, Task
 
-# Register your models here.
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = [
+        "content",
+        "datetime",
+        "deadline",
+        "is_done"
+    ]
