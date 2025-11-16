7f41bd3a52f33e9da9543538de4d15e7d33ae855
JustVov4uk <volodabudzan4@gmail.com>
2025-11-16 11:31:35 +0100

    Create index view in views.py. Add index view to config urls. Create index.html

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 
-urlpatterns = []
+from config.views import index
+
+urlpatterns = [
+    path("", index, name="index"),
+]
 
 app_name = "config"
--- config/views.py
+++ config/views.py
@@ -1,3 +1,13 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from config.models import Task
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    num_tasks = Task.objects.count()
+
+    context = {"num_tasks": num_tasks}
+
+    return render(request, "config/index.html")
+
