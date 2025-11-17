38622846fce2e02cc2e743b2cac0f52b1e6a8aba
JustVov4uk <volodabudzan4@gmail.com>
2025-11-17 11:40:11 +0100

    create TagCreate, TagUpdate, TagDeleteviews

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from config.views import index, TaskListView, TagListView
+from config.views import index, TaskListView, TagListView, TagCreateView, TagUpdateView, TagDeleteView
 
 urlpatterns = [
     path("", index, name="index"),
     path("tasks/", TaskListView.as_view(), name="task-list"),
     path("tags/", TagListView.as_view(), name="tag-list"),
+    path("tags/create/", TagCreateView.as_view(), name="tag-create"),
+    path("tags/update/<int:pk>/", TagUpdateView.as_view(), name="tag-update"),
+    path("tags/delete/<int:pk>/", TagDeleteView.as_view(), name="tag-delete"),
 ]
 
 app_name = "config"
--- config/views.py
+++ config/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import generic
 
 from config.models import Task, Tag
@@ -18,3 +19,18 @@ class TaskListView(generic.ListView):
 
 class TagListView(generic.ListView):
     model = Tag
+
+
+class TagCreateView(generic.CreateView):
+    model = Tag
+    success_url = reverse_lazy("config:tag-list")
+
+
+class TagUpdateView(generic.UpdateView):
+    model = Tag
+    success_url = reverse_lazy("config:tag-list")
+
+
+class TagDeleteView(generic.DeleteView):
+    model = Tag
+    success_url = reverse_lazy("config:tag-list")
